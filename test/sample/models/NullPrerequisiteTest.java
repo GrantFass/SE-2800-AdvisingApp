481ac29b58be5558c811f7cde755adb8e1535ba0
Hunter Turcin <turcinh@msoe.edu>
2021-03-16 23:07:43 -0500

    Add NullPrerequisite tests

---
+++ test/sample/models/NullPrerequisiteTest.java
@@ -0,0 +1,20 @@
+package sample.models;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class NullPrerequisiteTest {
+    Course ba1220 = new Course("BA1220", 3, new NullPrerequisite(), "Microeconomics");
+    Course bi102 = new Course("BI102", 4, new NullPrerequisite(), "Cell Biology and Genetics");
+
+    @Test
+    void testSatisfiedByNothing() {
+        assertTrue(ba1220.getPrerequisite().satisfiedBy(List.of()));
+    }
+
+    @Test
+    void testSatisfiedBySomething() {
+        assertTrue(ba1220.getPrerequisite().satisfiedBy(List.of(bi102)));
+    }
+}
