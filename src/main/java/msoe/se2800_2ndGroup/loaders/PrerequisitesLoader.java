7317739223e9842cad89ea26e49cfd853fa1999a
Hunter Turcin <turcinh@msoe.edu>
2021-03-23 14:21:17 -0500

    Add skeleton of PrerequisitesLoader

---
+++ src/main/java/msoe/se2800_2ndGroup/loaders/PrerequisitesLoader.java
@@ -0,0 +1,40 @@
+package msoe.se2800_2ndGroup.loaders;
+
+import java.io.Reader;
+
+import org.apache.commons.CSVParser;
+
+/**
+ * Project Authors: Fass, Grant; Poptile, Claudia; Toohill, Teresa; Turcin, Hunter;
+ * Class: SE 2800 041
+ * Group: 2
+ * Term: Spring 2020 - 2021
+ * Instructor: Dr. Magaña
+ * Affiliation: Milwaukee School of Engineering (MSOE)
+ * Project Name: AdvisingApp
+ * Class Name: PrerequisitesLoader
+ * Description:
+ * * Creates model objects from a prerequisites CSV document.
+ * The PrerequisitesLoader class is responsible for:
+ * * loading a prerequisites CSV
+ * Modification Log:
+ * * File Created by turcinh on Sunday, 21 March 2021
+ * <p>
+ * Copyright (C): TBD
+ *
+ * @author : turcinh
+ * @since : Sunday, 21 March 2021
+ */
+public class PrerequisitesLoader {
+    private final CSVParser parser;
+
+    /**
+     * Create a new prerequisites CSV document loader.
+     *
+     * @author : Hunter Turcin
+     * @since : Sun, 21 Mar 2021
+     * @param reader CSV data source
+     */
+    public PrerequisitesLoader(Reader reader) {
+    }
+}
