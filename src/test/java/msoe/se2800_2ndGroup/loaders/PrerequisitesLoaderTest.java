40293d2c1e656d5807975b697df450dd8e12c047
Hunter Turcin <turcinh@msoe.edu>
2021-03-31 15:00:15 -0500

    Add first data point for PrerequisitesLoaderTest
    References: #16

--- src/test/java/msoe/se2800_2ndGroup/loaders/PrerequisitesLoaderTest.java
+++ src/test/java/msoe/se2800_2ndGroup/loaders/PrerequisitesLoaderTest.java
@@ -1,20 +1,41 @@
 package msoe.se2800_2ndGroup.loaders;
 
 import msoe.se2800_2ndGroup.models.Course;
-import org.junit.BeforeClass;
+import msoe.se2800_2ndGroup.models.NullPrerequisite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collection;
 
+@RunWith(Parameterized.class)
 class PrerequisitesLoaderTest {
     static Collection<Course> courses;
 
-    @BeforeClass
-    public static void setUp() throws IOException {
+    private String code;
+    private Course course;
+
+    @Parameters
+    static Collection<Object[]> parameters() throws IOException {
         InputStreamReader reader = new InputStreamReader(PrerequisitesLoaderTest.class.getResourceAsStream("prerequisites_varied.csv"));
         PrerequisitesLoader loader = new PrerequisitesLoader(reader);
         courses = loader.load();
+
+        return Arrays.asList(new Object[][] {
+                {
+                    "TEST1", new Course("TEST1", 0, new NullPrerequisite(), "Test Course")
+                }
+        });
+    }
+
+    @Test
+    void testCourseEquals() {
+        assertEquals(course, getCourse(code));
     }
 
     private static Course getCourse(String code) {
