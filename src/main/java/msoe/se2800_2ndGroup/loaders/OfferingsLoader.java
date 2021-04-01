5897fcebe92dda9db5506cc37cb951dee60984f4
Hunter Turcin <turcinh@msoe.edu>
2021-04-01 12:47:36 -0500

    Fix header map
    References: #16

--- src/main/java/msoe/se2800_2ndGroup/loaders/OfferingsLoader.java
+++ src/main/java/msoe/se2800_2ndGroup/loaders/OfferingsLoader.java
@@ -50,12 +50,12 @@ public class OfferingsLoader {
      * @since : Mon, 29 Mar 2021
      */
     public OfferingsLoader(Reader reader, Collection<Course> courses) throws IOException {
-        this.parser = CSVFormat.DEFAULT.parse(reader);
+        this.parser = CSVFormat.DEFAULT.withHeader().parse(reader);
         this.courses = courses;
     }
 
     public Collection<Offering> load() {
-        final var majors = parser.getHeaderNames();
+        final var majors = new ArrayList<>(parser.getHeaderNames());
         majors.remove(COURSE);
         final var offerings = new ArrayList<Offering>();
 
