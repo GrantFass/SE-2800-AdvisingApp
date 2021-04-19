f40507c86f9993d8a0772b72d5d699355cda6d9f
Hunter Turcin <turcinh@msoe.edu>
2021-04-19 13:50:01 -0500

    Clean up CurriculumLoader

--- src/main/java/msoe/se2800_2ndGroup/loaders/CurriculumLoader.java
+++ src/main/java/msoe/se2800_2ndGroup/loaders/CurriculumLoader.java
@@ -6,12 +6,14 @@ import msoe.se2800_2ndGroup.models.CurriculumItem;
 import msoe.se2800_2ndGroup.models.Elective;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Project Authors: Fass, Grant; Poptile, Claudia; Toohill, Teresa; Turcin, Hunter;
@@ -30,6 +32,7 @@ import java.util.HashMap;
  * * File Created by turcinh on Sunday, 29 March 2021
  * * Modify javadoc for constructor slightly by Grant Fass on Tue, 30 Mar 2021
  * * code cleanup using JDK 16 features done by Hunter Turcin on 2021-04-07
+ * * code cleanup from group feedback by turcinh on Monday, 19 April 2021
  * <p>
  * Copyright (C): TBD
  *
@@ -43,9 +46,11 @@ public class CurriculumLoader {
     /**
      * Create a new curriculum CSV loader.
      *
+     * Courses must be loaded from {@see PrerequisitesLoader} first.
+     *
      * @param reader The Reader object created with a FileReader containing the path to the file to be read.
      * @param courses courses to use
-     * @author : First last
+     * @author : Hunter Turcin
      * @since : Mon, 29 Mar 2021
      */
     public CurriculumLoader(Reader reader, Collection<Course> courses) throws IOException {
@@ -57,6 +62,8 @@ public class CurriculumLoader {
      * Create model classes from a curriculum file.
      *
      * @return all curricula
+     * @author : Hunter Turcin
+     * @since : Mon, 29 Mar 2021
      */
     public Collection<Curriculum> load() {
         // major to curriculum items
@@ -66,40 +73,66 @@ public class CurriculumLoader {
             map.put(header, new ArrayList<>());
         }
 
-        final var majors = map.keySet();
-
         for (final var record : parser) {
-            for (final var major : majors) {
-                final var code = record.get(major);
-                CurriculumItem item = null;
-
-                for (final var course : courses) {
-                    if (course.code().equals(code)) {
-                        item = course;
-                        break;
-                    }
-                }
-
-                if (item == null && !code.isEmpty()) {
-                    item = new Elective(code);
-                }
-
-                if (item != null) {
-                    map.get(major).add(item);
-                }
-            }
+            parseRecord(record, map);
         }
 
-        final var curriculums = new ArrayList<Curriculum>();
+        final var curricula = new ArrayList<Curriculum>();
 
         for (final var entry : map.entrySet()) {
             final var major = entry.getKey();
             final var items = entry.getValue();
             final var curriculum = new Curriculum(major, items);
 
-            curriculums.add(curriculum);
+            curricula.add(curriculum);
         }
 
-        return curriculums;
+        return curricula;
+    }
+
+    /**
+     * Use a record to update curriculum item lists.
+     *
+     * @param record current record
+     * @param map map of major code to curriculum items
+     * @author : Hunter Turcin
+     * @since : Mon, 19 Apr 2021
+     */
+    private void parseRecord(CSVRecord record, Map<String, ArrayList<CurriculumItem>> map) {
+        final var majors = map.keySet();
+
+        for (final var major : majors) {
+            final var code = record.get(major);
+            final var item = getCurriculumItem(code);
+
+            if (item != null) {
+                map.get(major).add(item);
+            }
+        }
+    }
+
+    /**
+     * Get a course or elective from a code.
+     *
+     * @param code code of the course or elective
+     * @return the course or elective, or null if not found
+     * @author : Hunter Turcin
+     * @since : Mon, 19 Apr 2021
+     */
+    private CurriculumItem getCurriculumItem(String code) {
+        CurriculumItem item = null;
+
+        for (final var course : courses) {
+            if (course.code().equals(code)) {
+                item = course;
+                break;
+            }
+        }
+
+        if (item == null && !code.isEmpty()) {
+            item = new Elective(code);
+        }
+
+        return item;
     }
 }
