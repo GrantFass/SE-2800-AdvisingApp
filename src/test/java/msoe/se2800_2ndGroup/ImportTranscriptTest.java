c14a4e064af64f9e7ef380635e7e2e7f3ab0e0bb
cpoptile <77410366+cpoptile@users.noreply.github.com>
2021-04-21 00:03:44 -0500

    Create test class for Import Transcripts
    Create test stubs for each method in ImportTranscripts.java
    
    References: #22

---
+++ src/test/java/msoe/se2800_2ndGroup/ImportTranscriptTest.java
@@ -0,0 +1,58 @@
+package msoe.se2800_2ndGroup;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ImportTranscriptTest {
+
+    /**
+     * TODO
+     *
+     * @author : First Last
+     * @since : Tue, 20 Apr 2021
+     */
+    @BeforeEach
+    void setUp() {
+        //probably import sample files
+    }
+
+    /**
+     * TODO
+     *
+     * @author : First last
+     * @since : Tue, 20 Apr 2021
+     */
+    @Test
+    void checkLineForIgnoredWordsAndFailedClassesAndWithdrawnClasses(){
+        //TODO: changed method name, should be updated once merged
+    };
+
+    /**
+     * TODO
+     *
+     * @author : First last
+     * @since : Tue, 20 Apr 2021
+     */
+    @Test
+    void checkStringForCourseCode(){
+        //region Contains course code
+
+        //endregion
+
+        //region Does not contain course code
+
+        //endregion
+
+    }
+
+    /**
+     * TODO
+     *
+     * @author : First last
+     * @since : Tue, 20 Apr 2021
+     */
+    @Test
+    void readInFile(){
+        // TODO: figure out how to test overloaded methods :(
+    }
+}
